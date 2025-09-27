59b9fbe2eecb7ffcc98d17b06f56366060a67f79
SofiePedersen <sofie@gefiber.dk>
2025-09-27 20:17:40 +0200

    Update three-two.js

--- Opgave 3.2/three-two.js
+++ Opgave 3.2/three-two.js
@@ -1,51 +1,80 @@
 const canLiftMjolnir = [
   {
-    heroName: "Thor", alterEgo: "Thor Odinson", age: 1000, sex: "male",
+    heroName: "Thor", 
+    alterEgo: "Thor Odinson", 
+    age: 1000, sex: "male",
     powers: ['Superhuman strength', 'Weather manipulation', 'Flight', 'Longevity'],
     description: "Thor is a founding member of the Avengers and is known for his hammer Mjolnir."
   },
   {
-    heroName: "Iron Man", alterEgo: "Tony Stark", age: 45, sex: "male",
+    heroName: "Iron Man", 
+    alterEgo: "Tony Stark", 
+    age: 45, 
+    sex: "male",
     powers: ['Powered armor suit', 'Genius intellect', 'Flight', 'Energy blasts'],
     description: "A genius billionaire who built the Iron Man suit."
   },
   {
-    heroName: "Hulk", alterEgo: "Bruce Banner", age: 40, sex: "male",
+    heroName: "Hulk", 
+    alterEgo: "Bruce Banner", 
+    age: 40, 
+    sex: "male",
     powers: ['Superhuman strength', 'Regeneration', 'Durability'],
     description: "Transforms into the Hulk when angry."
   },
   {
-    heroName: "Captain America", alterEgo: "Steve Rogers", age: 100, sex: "male",
+    heroName: "Captain America", 
+    alterEgo: "Steve Rogers", 
+    age: 100, 
+    sex: "male",
     powers: ['Enhanced strength', 'Agility', 'Indomitable will'],
     description: "The first Avenger with a vibranium shield."
   },
   {
-    heroName: "Black Widow", alterEgo: "Natasha Romanoff", age: 35, sex: "female",
+    heroName: "Black Widow", 
+    alterEgo: "Natasha Romanoff", 
+    age: 35, 
+    sex: "female",
     powers: ['Martial arts', 'Espionage', 'Peak human condition'],
     description: "Master spy and assassin, member of the Avengers."
   },
   {
-    heroName: "Hawkeye", alterEgo: "Clint Barton", age: 38, sex: "male",
+    heroName: "Hawkeye", 
+    alterEgo: "Clint Barton", 
+    age: 38, 
+    sex: "male",
     powers: ['Archery', 'Marksmanship', 'Agility'],
     description: "The sharpshooter of the Avengers."
   },
   {
-    heroName: "Wonder Woman", alterEgo: "Diana Prince", age: 5000, sex: "female",
+    heroName: "Wonder Woman", 
+    alterEgo: "Diana Prince", 
+    age: 5000, 
+    sex: "female",
     powers: ['Superhuman strength', 'Flight', 'Combat skills'],
     description: "Amazonian warrior princess."
   },
   {
-    heroName: "Superman", alterEgo: "Clark Kent", age: 35, sex: "male",
+    heroName: "Superman", 
+    alterEgo: "Clark Kent", 
+    age: 35, 
+    sex: "male",
     powers: ['Super strength', 'X-ray vision', 'Flight'],
     description: "The Man of Steel from Krypton."
   },
   {
-    heroName: "Flash", alterEgo: "Barry Allen", age: 28, sex: "male",
+    heroName: "Flash", 
+    alterEgo: "Barry Allen", 
+    age: 28, 
+    sex: "male",
     powers: ['Super speed', 'Time travel', 'Accelerated healing'],
     description: "Fastest man alive."
   },
   {
-    heroName: "Spider-Man", alterEgo: "Peter Parker", age: 21, sex: "male",
+    heroName: "Spider-Man", 
+    alterEgo: "Peter Parker", 
+    age: 21, 
+    sex: "male",
     powers: ['Wall-crawling', 'Spider-sense', 'Super strength'],
     description: "Friendly neighborhood Spider-Man."
   }
